08fbbbf09d4e9778965647eedb49b9b04d522f35
rafaric <rafaelstrongoli@gmail.com>
2024-02-01 14:35:13 -0300

    corregido error de inicio.

--- src/helpers/getFromApi.ts
+++ src/helpers/getFromApi.ts
@@ -1,22 +1,45 @@
-import { PokemonInfo } from "../Interfaces/PokemonsInterface";
-interface Pokemon {
+export interface Welcome {
+  id: number;
   name: string;
-  url: string;
+  image?: Image;
+  weight: number;
+  height: number;
+}
+
+export interface Image {
+  sprites: Sprites;
+}
+
+export interface Sprites {
+  front_default: string;
 }
 
 export const getPokemones = async () => {
-  const lista: PokemonInfo[] = [];
-  await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
-    .then((response) => response.json())
-    .then((data) => data)
-    .then((result) => {
-      result.results.map((poke: Pokemon) => {
-        getPokemonInfo(poke.url).then((result) => {
-          lista.push(result);
-        });
-      });
-    });
-  return lista;
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
+  const data = await response.json();
+
+  const promises = data.results.map(
+    async (result: { name: string; url: string }) => {
+      const pokemonResponse = await fetch(result.url);
+      const pokemon = await pokemonResponse.json();
+
+      return {
+        id: pokemon.id,
+        order: pokemon.order,
+        name: pokemon.name,
+        weight: pokemon.weight,
+        height: pokemon.height,
+        image: pokemon.sprites.other.dream_world.front_default,
+        stats: pokemon.stats,
+        types: pokemon.types,
+        abilities: pokemon.abilities,
+        moves: pokemon.moves,
+        species: pokemon.species,
+      };
+    }
+  );
+
+  return Promise.all(promises);
 };
 
 export const getPokemonInfo = async (url: string) => {
